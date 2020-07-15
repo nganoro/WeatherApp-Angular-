2a7cb1f21f27f76d5acef5ab5096a6468c437f04
nganoro <50967358+nganoro@users.noreply.github.com>
2020-07-15 16:21:45 -0400

    Angular Weather app

---
+++ src/app/Components/weather-widget/weather-widget.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-weather-widget',
+  templateUrl: './weather-widget.component.html',
+  styleUrls: ['./weather-widget.component.css']
+})
+export class WeatherWidgetComponent implements OnInit {
+  
+  WeatherData:any;
+  constructor() { }
+
+  ngOnInit() {
+    this.WeatherData = {
+      main : {},
+      isDay: true
+    };
+    this.getWeatherData();
+    console.log(this.WeatherData);
+  }
+
+  getWeatherData(){
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=f951755259cf5507487bb52996874347')
+    .then(response=>response.json())
+    .then(data=>{this.setWeatherData(data);})
+  }
+
+  setWeatherData(data){
+    this.WeatherData = data;
+    let sunsetTime = new Date(this.WeatherData.sys.sunset * 1000);
+    this.WeatherData.sunset_time = sunsetTime.toLocaleString();
+    let currentDate = new Date();
+    this.WeatherData.isDay = (currentDate.getTime() < sunsetTime.getTime());
+    this.WeatherData.temp_celcius = (this.WeatherData.main.temp - 273.15).toFixed(0);
+    this.WeatherData.temp_min = (this.WeatherData.main.temp_min - 273.15).toFixed(0);
+    this.WeatherData.temp_max = (this.WeatherData.main.temp_max - 273.15).toFixed(0);
+    this.WeatherData.temp_feels_like = (this.WeatherData.main.feels_like - 273.15).toFixed(0);
+    this.WeatherData.country = this.WeatherData.sys.country.toLocaleString();
+  }
+
+}
